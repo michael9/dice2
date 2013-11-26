10dd696909af3b3c42df8be11fe724b797233f7a
michael carlos <michael82carlos@gmail.com>
2013-11-26 16:12:54 +0800

    左边抽屉

--- src/alialicoo/com/dice/MainActivity.java
+++ src/alialicoo/com/dice/MainActivity.java
@@ -11,14 +11,18 @@ import android.os.Message;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.SlidingDrawer;
 import android.widget.AdapterView.OnItemClickListener;
@@ -74,8 +78,12 @@ public class MainActivity extends Activity {
 	
 	SensorManager mSensorManager;// 声明一个SensorManager
 	Sensor accSensor = null;
-	SlidingDrawer sd = null;
+//	SlidingDrawer sd = null;
 	GridView mgv = null;
+	DrawerLayout dl=null; 
+	ListView menu_listview=null;
+	 public static final String[] TITLES =
+		    { "Henry IV (1)", "Henry V", "Henry VIII", "Richard II", "Richard III", "Merchant of Venice", "Othello", "King Lear" };
 	dice_view_bean[] dices_view = new dice_view_bean[Commdata.maxdices];
 
 	@Override
@@ -101,6 +109,7 @@ public class MainActivity extends Activity {
 		
 		Commdata.copydbfile();
 		Commdata.read_dice_db();
+		
 		  int i;
 	        int dicesize = (Commdata.screenHeight - 30) / 3;
 	        for (i = 0; i < Commdata.maxdices; i++) {
@@ -110,6 +119,7 @@ public class MainActivity extends Activity {
 	        }
 		findViews();
 		ini_ds();
+		setgrid();
 	}
 
 	void ini_ds() {
@@ -126,13 +136,20 @@ public class MainActivity extends Activity {
 	}
 
 	private void findViews() {
+	dl=(DrawerLayout)findViewById(R.id.drawer_layout);
+
+	 // Set the adapter for the list view
+//	 menu_listview.setAdapter(new ArrayAdapter<String>(this,
+//            R.layout.drawer_list_item, mPlanetTitles));
+  
+    
 		// main_ll = (LinearLayout) findViewById(R.id.main_ll);
 		// main_ll.setDrawingCacheEnabled(true);
 		// tplayout = (RelativeLayout) findViewById(R.id.tplayout);
 		// tplayout.setDrawingCacheEnabled(true);
 		// dplayout = (RelativeLayout) findViewById(R.id.dplayout);
 		mgv = (GridView) findViewById(R.id.dice_slist);
-		sd = (SlidingDrawer) findViewById(R.id.sd_dices);
+//		sd = (SlidingDrawer) findViewById(R.id.sd_dices);
 //		sd.open();
 		// helpbutton = (ImageButton) findViewById(R.id.helpbutton);
 		// camerashare = (ImageView) findViewById(R.id.camerashare);
@@ -158,58 +175,108 @@ public class MainActivity extends Activity {
 	}
 
 	private void setCommondListener() {
-		/*
-		 * dices_view[0].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[1].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[2].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[3].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[4].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[5].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[6].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[7].div.setOnLongClickListener(new Uilclicked());
-		 * dices_view[8].div.setOnLongClickListener(new Uilclicked());
-		 * 
-		 * dices_view[0].div.setOnClickListener(new UIclicked());
-		 * dices_view[1].div.setOnClickListener(new UIclicked());
-		 * dices_view[2].div.setOnClickListener(new UIclicked());
-		 * dices_view[3].div.setOnClickListener(new UIclicked());
-		 * dices_view[4].div.setOnClickListener(new UIclicked());
-		 * dices_view[5].div.setOnClickListener(new UIclicked());
-		 * dices_view[6].div.setOnClickListener(new UIclicked());
-		 * dices_view[7].div.setOnClickListener(new UIclicked());
-		 * dices_view[8].div.setOnClickListener(new UIclicked());
-		 * mgv.setOnItemClickListener(new OnItemClickListener() {
-		 * 
-		 * @Override public void onItemClick(AdapterView<?> arg0, View arg1, int
-		 * arg2, long arg3) { if (arg2 < Commdata.dices.size()) { adddice(arg2);
-		 * // showanim(); } else { Intent intent = new Intent(Dice.this,
-		 * Add_page.class); startActivity(intent);
-		 * overridePendingTransition(R.anim.activity_slide_r2l,
-		 * R.anim.activity_slide_l2r); } sd.close(); } });
-		 * 
-		 * mgv.setOnItemLongClickListener(new OnItemLongClickListener() {
-		 * 
-		 * @Override public boolean onItemLongClick(AdapterView<?> arg0, View
-		 * arg1, int arg2, long arg3) { // TODO Auto-generated method stub if
-		 * (arg2 < Commdata.dices.size()) { delseln = arg2; Intent intent = new
-		 * Intent(Dice.this, CMSGDlg.class); intent.putExtra("msg_id",
-		 * R.string.msg_give_up_the_dice); startActivityForResult(intent, 3); }
-		 * return false; } });
-		 * 
-		 * tplayout.setOnClickListener(new UIclicked());
-		 * helpbutton.setOnClickListener(new UIclicked()); //
-		 * camerashare.setOnClickListener(new UIclicked()); //
-		 * tableashare.setOnClickListener(new UIclicked());
-		 * sd.setOnDrawerOpenListener(new OnDrawerOpenListener() {
-		 * 
-		 * @Override public void onDrawerOpened() { // TODO Auto-generated
-		 * method stub mgv.startLayoutAnimation(); } });
-		 * mainmenu_btn.setOnClickListener(new UIclicked()); // Ad //
-		 * adviewbar.setOnClickListener(new adclick());
+		
+		  dices_view[0].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[1].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[2].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[3].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[4].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[5].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[6].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[7].div.setOnLongClickListener(new Uilclicked());
+		  dices_view[8].div.setOnLongClickListener(new Uilclicked());
+		  
+		  dices_view[0].div.setOnClickListener(new UIclicked());
+		  dices_view[1].div.setOnClickListener(new UIclicked());
+		  dices_view[2].div.setOnClickListener(new UIclicked());
+		 dices_view[3].div.setOnClickListener(new UIclicked());
+		 dices_view[4].div.setOnClickListener(new UIclicked());
+		  dices_view[5].div.setOnClickListener(new UIclicked());
+		  dices_view[6].div.setOnClickListener(new UIclicked());
+		  dices_view[7].div.setOnClickListener(new UIclicked());
+		  dices_view[8].div.setOnClickListener(new UIclicked());
+		
+		  mgv.setOnItemClickListener(new OnItemClickListener() {
+			  public void onItemClick(android.widget.AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				  dl.closeDrawers();
+			  };
+		});
+		  /*
+		  mgv.setOnItemLongClickListener(new OnItemLongClickListener() {
+		  
+		  @Override public boolean onItemLongClick(AdapterView<?> arg0, View
+		  arg1, int arg2, long arg3) { // TODO Auto-generated method stub if
+		  (arg2 < Commdata.dices.size()) { delseln = arg2; Intent intent = new
+		  Intent(Dice.this, CMSGDlg.class); intent.putExtra("msg_id",
+		  R.string.msg_give_up_the_dice); startActivityForResult(intent, 3); }
+		  return false; } });
+		  
+		  tplayout.setOnClickListener(new UIclicked());
+		  helpbutton.setOnClickListener(new UIclicked()); //
+		  camerashare.setOnClickListener(new UIclicked()); //
+		  tableashare.setOnClickListener(new UIclicked());
+		  sd.setOnDrawerOpenListener(new OnDrawerOpenListener() {
+		  
+		  @Override public void onDrawerOpened() { // TODO Auto-generated
+		  method stub mgv.startLayoutAnimation(); } });
+		  mainmenu_btn.setOnClickListener(new UIclicked()); // Ad //
+		  adviewbar.setOnClickListener(new adclick());
 		 */
 	}
 
+    void setgrid() {
+//        Commdata.refsdl = false;
+//        Commdata.read_dice_db();
+        mgv.setAdapter(new ImageAdapter(this));
+    }
 	/******************************************************************************/
+    class Uilclicked implements OnLongClickListener {
+        @Override
+        public boolean onLongClick(View view) {
+            // TODO Auto-generated method stub
+            switch (view.getId()) {
+            case R.id.div1:
+                dices_view[0].div.setVisibility(8);
+                break;
+            case R.id.div2:
+                dices_view[1].div.setVisibility(8);
+                break;
+            case R.id.div3:
+                dices_view[2].div.setVisibility(8);
+                break;
+            case R.id.div4:
+                dices_view[3].div.setVisibility(8);
+                break;
+            case R.id.div5:
+                dices_view[4].div.setVisibility(8);
+                break;
+            case R.id.div6:
+                dices_view[5].div.setVisibility(8);
+                break;
+            case R.id.div7:
+                dices_view[6].div.setVisibility(8);
+                break;
+            case R.id.div8:
+                dices_view[7].div.setVisibility(8);
+                break;
+            case R.id.div9:
+                dices_view[8].div.setVisibility(8);
+                break;
+
+            default:
+                break;
+            }
+
+            return true;
+        }
+    }
+    
+    class UIclicked implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+         
+        }
+    };
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
